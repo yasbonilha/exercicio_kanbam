77d88a549a0d9213a9005cbf9ddacec7d7653f31
TAYNAH TAMIE MASUNAGA SAITO <23.00621-8@maua.br>
2024-03-14 10:58:45 -0300

    adição

---
+++ Adição.py
@@ -0,0 +1,8 @@
+operacao = int(input("qual operação você deseja?\n1. Soma\n2. Subtração\n3. Multiplicação\n4. Divisão"))
+
+a = int(input("Insira o primeiro valor: "))
+b = int(input("Insira o segundo valor: "))
+
+soma = a + b
+
+print("A soma dos valores é: ", soma)
